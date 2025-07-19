f72aab44fc8c4a8f34d78095157a7d5e5c82ba98
fbraza <fbraza@tutanota.com>
2025-07-19 20:08:43 +0200

    test: add values for unit test score2 diabetes

--- tests/test_score2_diabetes.py
+++ tests/test_score2_diabetes.py
@@ -10,12 +10,12 @@ OUT_FILEPATH = Path(__file__).parent / "inputs" / "score2_diabetes"
 @pytest.mark.parametrize(
     "filename,expected",
     [
-        ("test__input__patient_01.json", (45.00, 0.00, "PLACEHOLDER")),
-        ("test__input__patient_02.json", (55.00, 0.00, "PLACEHOLDER")),
-        ("test__input__patient_03.json", (65.00, 0.00, "PLACEHOLDER")),
-        ("test__input__patient_04.json", (48.00, 0.00, "PLACEHOLDER")),
-        ("test__input__patient_05.json", (60.00, 0.00, "PLACEHOLDER")),
-        ("test__input__patient_06.json", (42.00, 0.00, "PLACEHOLDER")),
+        ("test__input__patient_01.json", (45.00, 4.40, "High")),
+        ("test__input__patient_02.json", (55.00, 11.80, "Very high")),
+        ("test__input__patient_03.json", (65.00, 18.60, "Very high")),
+        ("test__input__patient_04.json", (48.00, 2.30, "Low to moderate")),
+        ("test__input__patient_05.json", (60.00, 22.30, "Very high")),
+        ("test__input__patient_06.json", (42.00, 5.80, "High")),
     ],
 )
 def test_score2_diabetes(filename, expected):
@@ -32,9 +32,8 @@ def test_score2_diabetes(filename, expected):
     expected_age, expected_risk, expected_category = expected
 
     assert age == expected_age
-    # TODO: Update these assertions once actual values are calculated
-    # assert pred_risk_category == expected_category
-    # assert pytest.approx(pred_risk, abs=0.1) == expected_risk
+    assert pred_risk_category == expected_category
+    assert pytest.approx(pred_risk, abs=0.1) == expected_risk
 
 
 if __name__ == "__main__":
